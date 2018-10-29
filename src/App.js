02577be9ad7f39a717eaa861a92dba40f0bd0e81
Tamar Auber <mischegoss@gmail.com>
2018-10-29 10:08:37 -0400

    refactor: updated search function

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css'
 
 class BooksApp extends Component {
   state = {
+    query: "",
     allBooks: [],
     filteredBooks: []
   }
@@ -24,12 +25,12 @@ class BooksApp extends Component {
     if (query) {
       BooksAPI
         .search(query)
-        .then((result) => {
-          this.updateSearch(result)
-          if (result.error !== 'empty query') {
-            this.setState({filteredBooks: result})
-          } else {
+        .then((filteredBooks) => {
+          this.updateQuery(filteredBooks)
+          if (filteredBooks.error) {
             this.setState({filteredBooks: []})
+          } else {
+            this.setState({filteredBooks})
           }
         })
     } else {
@@ -37,27 +38,22 @@ class BooksApp extends Component {
     }
   }
 
+updateQuery = (query) => {
+        this.setState({ query })
+        this.searchBooks(query)
+    }
 
   updateShelf = (book, shelf) => {
     BooksAPI
       .update(book, shelf)
       .then(updated => (BooksAPI.getAll().then((books) => {
         this.setState({allBooks: books})
-        this.updateSearch(this.state.filteredBooks)
+        this.updateQuery(this.state.filteredBooks)
       })))
   }
 
 
-  updateSearch = (values) => {
-    for (let value of values) {
-      for (let book of this.state.allBooks) {
-        if (value.id === book.id) {
-          value.shelf = book.shelf
-        }
-      }
-                   }
-    this.setState({filteredBooks: values})
-  }
+
 
   render() {
     return (
@@ -81,16 +77,6 @@ class BooksApp extends Component {
                 updateOption={(book, shelf) => this.updateShelf(book, shelf)}/>
             </div>
           )}/>
-
-          <Route
-            component={function NoMatch() {
-            return (
-              <div className="errorPage">
-                <h1>404</h1>
-                <h3>Page not Found</h3>
-              </div>
-            )
-          }}/>
 
         </Switch>
       </div>
